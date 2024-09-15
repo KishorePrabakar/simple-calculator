88cc0077003859aa86e9117ae191607863dea93a
Kraxon <kishorefun21@gmail.com>
2024-09-16 05:27:27 +0530

    Upgraded to four arithmetic operations

--- Calc.java
+++ Calc.java
@@ -7,7 +7,30 @@ public class Calc{
 		int num1 = in.nextInt();
 		System.out.println("Enter Number 2: ");
 		int num2 = in.nextInt();
-		int sum = num1 + num2;
-		System.out.println("The sum is " + sum);
+		System.out.println("Enter your choice of operation[+, -, *, /]: ");
+		char ch = in.next().charAt(0);
+		int result;
+
+		switch(ch){
+			case '+':
+				result = num1 + num2;
+				break;
+			case '-':
+				result = num1 - num2;
+				break;
+			case '*':
+				result = num1 * num2;
+				break;
+			case '/':
+				if (num2 != 0){
+				result = num1 / num2;
+				} else{
+					System.out.println("Error! Check Input");
+					return;}
+				break;
+			default:
+				System.out.println("Error! Operation is not correct.");
+				return;}
+		System.out.println("The result is " + result);
 	}
 }
